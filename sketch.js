8d999b2f130558f37a49cc4fbe55d6c325ab2042
zavierwise <zavierwise@verizon.net>
2023-12-04 07:02:24 -0500

    added image finished menu
    menu done

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 let programState = 'menu';
 
+
+function preload() {
+  myFont1 = loadFont('DemonWings-GOn4Z.ttf');
+}
+
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
   rectMode(CENTER);
-  canvas.parent('sketch-holder');
+  textAlign(CENTER);
+  //canvas.parent('sketch-holder');
 }
 
 function draw() {
@@ -14,6 +21,9 @@ function draw() {
     case 'game':
       gameScreen();
       break;
+    case 'info':
+      infoScreen();
+      break;
     case 'score':
       scoreScreen();
       break;
@@ -22,14 +32,40 @@ function draw() {
 
 function menuScreen() {
 //set up
-background(220);
+background(0);
   
 //title
 fill(128,0,6);
-textSize(20);
-
+textSize(100);
+textFont(myFont1);
+text('SPEED DEMON',width*0.46,height*0.5);
 
 //start button
+if ((mouseX > width*0.4) && (mouseX < width*0.62) && (mouseY > height*0.625) && (mouseY < height*0.71)) {
+  fill(255,0,0);
+  textSize(50);
+  textFont(myFont1);
+  text('START', width*0.5,height*0.7);
+} else {
+  fill(255);
+  textSize(50);
+  textFont(myFont1);
+  text('START', width*0.5,height*0.7);
+}
+  
+//info button
+if ((mouseX > width*0.44) && (mouseX < width*0.58) && (mouseY > height*0.78) && (mouseY < height*0.85)) {
+  fill(255,0,0);
+  textSize(50);
+  textFont(myFont1);
+  text('INFO', width*0.505,height*0.85);
+} else {
+  fill(255);
+  textSize(50);
+  textFont(myFont1);
+  text('INFO', width*0.505,height*0.85);
+  
+}
   
 }
 
@@ -43,8 +79,11 @@ function scoreScreen() {
 
 }
 
+
 function mouseReleased() {
-  if ((programState = 'menu') && ((mouseX > width*0.738) && (mouseX < width*0.899) && (mouseY > height*0.035) && (mouseY < height*0.09))) {
+  if ((programState = 'menu') && ((mouseX > width*0.4) && (mouseX < width*0.62) && (mouseY > height*0.625) && (mouseY < height*0.71))) {
     programState = 'game';
+  } else if ((programState = 'menu') && ((mouseX > width*0.44) && (mouseX < width*0.58) && (mouseY > height*0.78) && (mouseY < height*0.85))) {
+    programState = 'info';
   }
 }
